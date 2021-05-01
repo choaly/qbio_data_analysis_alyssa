3858d82c3a001b50fbc2ec4d92afd4fae6bc3c72
Alyssa Cho <choalyssa3@gmail.com>
2021-05-01 16:34:12 -0700

    cleaned up final_project code files

--- final_project/spearman_plots.py
+++ final_project/spearman_plots.py
@@ -5,47 +5,35 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats 
 
-# cptac.download(dataset="Brca")
+#---------------RNA–protein Spearman correlation analysis-------------#
+cptac.download(dataset="Brca")
 br = cptac.Brca()
 
 protein_data = br.get_proteomics()
 
-#The dataframes are MultIndex pandas dataframes. 
-#However, to teach the basics of pandas, we will remove the "multi" part of the dataframe.
+#Remove the "multi" part of the dataframe (the dataframes are MultIndex pandas dataframes)
 protein_data = protein_data.droplevel(1, axis=1)
 
 rna_data = br.get_transcriptomics()
 clinical_data = br.get_clinical()
 
-clinical_data["Age_in_years"] = clinical_data["Age.in.Month"]/12 #fill in with correct math
+clinical_data["Age_in_years"] = clinical_data["Age.in.Month"]/12
 
 
 # clinical_data.to_csv("~/Desktop/qbioresearch/qbio_data_analysis_alyssa/data/clinical_data.csv")
 # protein_data.to_csv("~/Desktop/qbioresearch/qbio_data_analysis_alyssa/data/protein_data.csv")
 # rna_data.to_csv("~/Desktop/qbioresearch/qbio_data_analysis_alyssa/data/rna_data.csv")
-# #Read back in your data
-# #Try with different arguments. The index_col=0 creates the index (rownames) as the first column. 
-# #Double check that the index and columns are what you want
-# clinical_data_readin = pd.read_csv("~/Desktop/qbioresearch/qbio_data_analysis_alyssa/data/clinical_data.csv", index_col=1)
-# clinical_data_readin
+
+# clinical_data_readin = pd.read_csv("~/Desktop/qbioresearch/qbio_data_analysis_alyssa/data/clinical_data.csv", index_col=1) #The index_col=0 creates the index (rownames) as the first column. 
 
 
-#Step 1: Check that the patients are in the same order.
-#This is important because we are looking at the gene expression and protein information of EACH patient
-#The data needs to be in pairs. This is easiest when the patients are in the same order in rna_data and protein_data
-
-#Are the index or columns (rna_data.index or rna_data.columns) the patients?
-#A: rna_data.index
-
-#Write an assert statement that checks the patients rna_data are equal to the patients of protein_data 
-#Fill in parantheses 
+#Check that the patients are in the same order in rna_data and protein_data.
+#We are looking at the gene expression and protein information of EACH patient, so the data needs to be in pairs.
+#rna_data.index are the patients (not rna_data.columns)
 assert list(protein_data.index) == list(rna_data.index)
 
 
-
-#Plot the data
-#You can present the rho value in various ways. Table, scatterplot, etc. 
-#Here you will make a scatterplot
+#Creates scatter plots presenting the Spearman correlation rho value
 def spearman_plots (gene_name, gene_rna_data, gene_protein_data, output_file):
     '''make scatterplots showing correlation between transcriptomic and proteomic data'''
 
@@ -63,21 +51,16 @@ def spearman_plots (gene_name, gene_rna_data, gene_protein_data, output_file):
     title = "rho: {} for {}".format(rho, gene_name)
     plt.title(title)
 
-    #Fill in informative x and y labels
-    plt.xlabel("RNA") #how do we know RNA is x-axis, not y-axis?  plt.scatter( gene_rna_data, gene_protein_data )
+    plt.xlabel("RNA")
     plt.ylabel("Protein")
 
     pngfile = '{}.png'.format( output_file)
 
-    plt.savefig( pngfile, bbox_inches="tight" ) #Use this when saving figure in script (use plot.show() in jupyter ntbk)
+    plt.savefig( pngfile, bbox_inches="tight" ) #Use plt.savefig() when saving figure in script (use plt.show() in jupyter ntbk)
 
 
-#Access the transcriptomic and proteomic information of the specific gene (ESR1)
-
-#Fill in the brackets []. We want ALL rows of the [specifc gene] column. 
-#Remember [row,col] format and : refers to ALL.
-
-rna_pik3ca = rna_data.loc[:, "PIK3CA"]
+#Access the transcriptomic and proteomic information of the specific genes (PIK3CA and TP53)
+rna_pik3ca = rna_data.loc[:, "PIK3CA"] #want all rows of the [specifc gene] column. 
 protein_pik3ca = protein_data.loc[:, "PIK3CA"]
 spearman_plots("PIK3CA", rna_pik3ca, protein_pik3ca, "pik3ca_spear")
 
@@ -87,7 +70,7 @@ spearman_plots("TP53", rna_tp53, protein_tp53, "tp53_spear")
 
 
 
-# #----------UNNECESSARY--DROPPED TTN. Test if TTN has null values-----------
+# #----------ABANDONED CODE (no longer investigating TTN). Test if TTN has null values-----------
 # rna_ttn = rna_data.loc[:, "TTN"]
 # protein_ttn = protein_data.loc[:, "TTN"]
 
